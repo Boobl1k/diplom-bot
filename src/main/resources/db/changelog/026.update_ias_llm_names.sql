8dbea793db412e0456b8a3ab7884cbc13fd2cbd0
Mansur <ahmatowmansur@gmail.com>
2024-05-20 16:59:48 +0300

    tuning draft

---
+++ src/main/resources/db/changelog/026.update_ias_llm_names.sql
@@ -0,0 +1,11 @@
+UPDATE public.dis_problem
+SET llm_name = 'Открытие и настройка доступа к модулям ИАС (информационно-аналитическая система Электронный университет)'
+WHERE id = 23;
+
+UPDATE public.dis_problem
+SET llm_name = 'Ошибка в работе ИАС (информационно-аналитическая система Электронный университет)'
+WHERE id = 22;
+
+UPDATE public.dis_problem
+SET llm_name = null
+WHERE id = 10;
