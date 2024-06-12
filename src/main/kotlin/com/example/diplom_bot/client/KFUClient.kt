e61302485ebe8c951c184321ea2f326e3385622a
Mansur <ahmatowmansur@gmail.com>
2024-06-12 20:00:47 +0300

    fixed context

--- src/main/kotlin/com/example/diplom_bot/client/KFUClient.kt
+++ src/main/kotlin/com/example/diplom_bot/client/KFUClient.kt
@@ -1,6 +1,6 @@
 package com.example.diplom_bot.client
 
-import com.example.diplom_bot.configuration.KfuClientConfiguration
+import com.example.diplom_bot.configuration.KFUClientConfiguration
 import com.example.diplom_bot.dto.external.RequestTypeListDTO
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam
     name = "KFU",
     url = "\${bot-properties.kfu-url}",
     path = "\${bot-properties.kfu-sub-path}",
-    configuration = [KfuClientConfiguration::class]
+    configuration = [KFUClientConfiguration::class]
 )
 interface KFUClient {
     @GetMapping("chatbot_api.get_list_request_type")
--- src/main/kotlin/com/example/diplom_bot/configuration/KFUClientConfiguration.kt
+++ src/main/kotlin/com/example/diplom_bot/configuration/KFUClientConfiguration.kt
@@ -4,7 +4,7 @@ import com.example.diplom_bot.property.ChatBotProperties
 import feign.RequestInterceptor
 import org.springframework.context.annotation.Bean
 
-class KfuClientConfiguration(
+class KFUClientConfiguration(
     private val chatBotProperties: ChatBotProperties
 ) {
     @Bean
